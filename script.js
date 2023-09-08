69500c501985c2ef89a949177362c8938004c8a9
Ved Prakash Dubey <vedprakashdubey528@gmail.com>
2023-09-08 16:19:17 +0530

    Weather app added

---
+++ script.js
@@ -0,0 +1,79 @@
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
+
+const location_not_found = document.querySelector('.location-not-found');
+
+const weather_body = document.querySelector('.weather-body');
+
+async function checkWeather(city){
+    // const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
+    const api_key="531032b383f80edf64f35f442b1e06bf";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    try{
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+    // if(weather_data.cod === `404`){
+    //     location_not_found.style.display = "flex";
+    //     weather_body.style.display = "none";
+    //     console.log("error");
+    //     return;
+    // }
+
+    console.log("run");
+     console.log(weather_data);
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+    
+    switch(weather_data.weather[0].main){
+        case 'Clouds':
+            weather_img.src = "images/cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src="images/clear.png"
+            break;
+        case 'Rain':
+            weather_img.src = "images/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "images/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "images/snow.png";
+            break;
+
+    }
+    }
+    catch(err){
+        console.log("Error is ",err);
+        // if(weather_data.cod === `404`){
+            location_not_found.style.display = "flex";
+            weather_body.style.display = "none";
+            console.log("error");
+            // return;
+        // }
+
+    }
+   
+}
+
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
+// var wage = document.getElementById("wage");
+inputBox.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"  e.code === "Enter"
+        checkWeather(inputBox.value);
+    }
+});
